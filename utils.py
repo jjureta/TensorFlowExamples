ec7817d32d8f2fee6b163d6f42d6dc22a46c0c6e
jjureta <josip.jureta@gmail.com>
2019-01-03 09:19:39 -0500

    New data set
    Use new MNIST data set from the keras

--- utils.py
+++ utils.py
@@ -8,5 +8,21 @@ def display_digit(data_set, num, prediction = -1):
     plt.imshow(image, cmap=plt.get_cmap('gray_r'))
     plt.show()
 
+# display an image from the given set
+def display_digit_v1(data_set, num, prediction = -1):
+    label = data_set[1][num]
+    image = data_set[0][num]
+    plt.title('Example: %d  Label: %d Prediction: %d' % (num, label, prediction))
+    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+    plt.show()
+
+# display an image from the given set
+def display_digit_v2(images, labels, num, prediction = -1):
+    label = labels[num]
+    image = images[num]
+    plt.title('Example: %d  Label: %d Prediction: %d' % (num, label, prediction))
+    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+    plt.show()
+
 def unfold_labels(labels):
     return labels.argmax(axis=1)
