b23302fdb9379b3f8a22c319eb1002a3b9774d16
jjureta <josip.jureta@gmail.com>
2019-01-04 20:42:31 -0500

    Use new mnist data set

---
+++ dataset_helper.py
@@ -0,0 +1,95 @@
+import numpy
+from tensorflow.python.framework import random_seed
+from keras.utils import to_categorical
+
+class DataSetHelper(object):
+    
+  def __init__(self,
+               images,
+               labels,
+               one_hot = False,
+               num_classes = 10,
+               rescale = False,
+               seed = None):
+    """Construct a DataSet.
+    one_hot arg is used only if fake_data is true.  `dtype` can be either
+    `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+    `[0, 1]`.  Seed arg provides for convenient deterministic testing.
+    """
+    seed1, seed2 = random_seed.get_seed(seed)
+    # If op level seed is not set, use whatever graph level seed is returned
+    numpy.random.seed(seed1 if seed is None else seed2)
+    
+    assert images.shape[0] == labels.shape[0], (
+                                                'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+    self._num_examples = images.shape[0]
+
+    # Convert shape from [num examples, rows, columns, depth]
+    # to [num examples, rows*columns] (assuming depth == 1)
+    images = images.reshape(images.shape[0],
+                            images.shape[1] * images.shape[2])
+       
+    if one_hot:
+        labels = to_categorical(labels, num_classes=num_classes)
+    # Convert from [0, 255] -> [0.0, 1.0].
+    if rescale:
+        images = images.astype(numpy.float32)
+        images = numpy.multiply(images, 1.0 / 255.0)
+
+    self._images = images
+    self._labels = labels
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def images(self):
+    return self._images
+
+  @property
+  def labels(self):
+    return self._labels
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+  @property
+  def epochs_completed(self):
+    return self._epochs_completed
+
+  def next_batch(self, batch_size, shuffle=True):
+    """Return the next `batch_size` examples from this data set."""
+    start = self._index_in_epoch
+    # Shuffle for the first epoch
+    if self._epochs_completed == 0 and start == 0 and shuffle:
+      perm0 = numpy.arange(self._num_examples)
+      numpy.random.shuffle(perm0)
+      self._images = self.images[perm0]
+      self._labels = self.labels[perm0]
+    # Go to the next epoch
+    if start + batch_size > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      # Get the rest examples in this epoch
+      rest_num_examples = self._num_examples - start
+      images_rest_part = self._images[start:self._num_examples]
+      labels_rest_part = self._labels[start:self._num_examples]
+      # Shuffle the data
+      if shuffle:
+        perm = numpy.arange(self._num_examples)
+        numpy.random.shuffle(perm)
+        self._images = self.images[perm]
+        self._labels = self.labels[perm]
+      # Start next epoch
+      start = 0
+      self._index_in_epoch = batch_size - rest_num_examples
+      end = self._index_in_epoch
+      images_new_part = self._images[start:end]
+      labels_new_part = self._labels[start:end]
+      return numpy.concatenate(
+          (images_rest_part, images_new_part), axis=0), numpy.concatenate(
+              (labels_rest_part, labels_new_part), axis=0)
+    else:
+      self._index_in_epoch += batch_size
+      end = self._index_in_epoch
+      return self._images[start:end], self._labels[start:end]
--- utils.py
+++ utils.py
@@ -1,28 +1,46 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+def display_digit(index, image, label, prediction):
+    image = image.reshape([28,28])
+    title = ['Sample:%d' % (index),
+             'Label: %d' % (label),
+             'Prediction: %d' % (prediction)]
+        
+    plt.title('\n'.join(title))
+    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
 
 # display an image from the given set
-def display_digit(data_set, num, prediction = -1):
-    label = data_set.labels[num].argmax(axis=0)
-    image = data_set.images[num].reshape([28,28])
-    plt.title('Example: %d  Label: %d Prediction: %d' % (num, label, prediction))
-    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+def display_errors(images, labels, predictions, grid = -1):
+    labels = labels.argmax(axis=1)
+    
+    errors = np.where(predictions != labels)[0]
+    
+    number_of_errors = errors.shape[0]
+    
+    if grid == -1:
+        random_error_indx = errors[np.random.randint(errors.size, size=1)[0]]
+        prediction = predictions[random_error_indx]
+        
+        label = labels[random_error_indx]
+        image = images[random_error_indx].reshape([28,28])
+        plt.title('Example: %d Label: %d Prediction: %d' % (random_error_indx, label, prediction))
+        plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+    else:
+        grid = grid if number_of_errors > grid else number_of_errors
+        fig = plt.figure()
+        fig.subplots_adjust(hspace=0.25, wspace=2)
+        for index in range(1, grid + 1):
+            random_error_indx = errors[np.random.randint(errors.size, size=1)[0]]
+            
+            plt.subplot(2, 4, index)
+            display_digit(random_error_indx,
+                          images[random_error_indx],
+                          labels[random_error_indx],
+                          predictions[random_error_indx])
+
     plt.show()
 
-# display an image from the given set
-def display_digit_v1(data_set, num, prediction = -1):
-    label = data_set[1][num]
-    image = data_set[0][num]
-    plt.title('Example: %d  Label: %d Prediction: %d' % (num, label, prediction))
-    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-    plt.show()
-
-# display an image from the given set
-def display_digit_v2(images, labels, num, prediction = -1):
-    label = labels[num]
-    image = images[num]
-    plt.title('Example: %d  Label: %d Prediction: %d' % (num, label, prediction))
-    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-    plt.show()
 
 def unfold_labels(labels):
     return labels.argmax(axis=1)
